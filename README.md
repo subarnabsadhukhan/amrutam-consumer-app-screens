4cd3686b45d854ed87d9a8cf9d455c5086caafcc
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-07-02 09:29:05 +0530

    deploy to vercel

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Amrutam Consumer App
+# Amrutam Forum Screens
+
+Welcome to the Amrutam Forum Screens repository! This repository contains the screens and UI components for the Amrutam Forum application.
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to="/commune">Go to Commune Section</Link>
+    </div>
+  );
 };
 
 export default Home;
